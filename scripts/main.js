b979a7908ad8b4c89689cb7a79d4dee68344acc0
PNY92 <99064526+PNY92@users.noreply.github.com>
2023-09-15 12:22:02 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,4 @@
 
 // VARIABLES \\
 
-const heading = document.getElementById("header");
-var i = 0;
-heading.style.opacity = 0
-function loop() {
-    setTimeout(() => {
-        heading.style.opacity = i;
-        i += 0.1;
-        if (i < 1){
-            loop()
-        }
-    },100)
-}
-
-loop()
+dataTable = {name: jq, pass: jqiang}
